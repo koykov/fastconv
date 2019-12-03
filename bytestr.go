dd7c1aa8923cc6fe33fb9e3b3cdc487ac8be8416
Konstantin Voykov <koykov0@gmail.com>
2019-12-04 00:42:18 +0200

    Add shorthand for bytes-string conversation functions.

--- bytestr.go
+++ bytestr.go
@@ -12,6 +12,11 @@ func BytesToString(b []byte) string {
 	return *(*string)(unsafe.Pointer(&b))
 }
 
+// Shorthand alias of BytesToString().
+func B2S(b []byte) string {
+	return BytesToString(b)
+}
+
 // Fast conversion of string to byte sequence.
 func StringToBytes(s string) []byte {
 	strh := (*reflect.StringHeader)(unsafe.Pointer(&s))
@@ -20,4 +25,9 @@ func StringToBytes(s string) []byte {
 	sh.Len = strh.Len
 	sh.Cap = strh.Len
 	return *(*[]byte)(unsafe.Pointer(&sh))
+}
+
+// Shorthand alias of StringToBytes().
+func S2B(s string) []byte {
+	return StringToBytes(s)
 }
