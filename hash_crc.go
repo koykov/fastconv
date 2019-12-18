a9a9f7613e5bb0255161c17aa638e57daa892eca
Konstantin Voykov <koykov0@gmail.com>
2019-12-18 17:02:46 +0200

    Tableless crc32 hash functions.

--- hash_crc.go
+++ hash_crc.go
@@ -1,3 +1,38 @@
 package fastconv
 
-// ...
+const (
+	Polynomial uint32 = 0xEDB88320
+
+	MaxSlice16    = 16
+	MaxSlice8     = 8
+	MaxSlice4     = 4
+	MaxSlice1     = 1
+	MaxSliceNoLut = 0
+)
+
+func swap(x uint32) uint32 {
+	return (x >> 24) |
+		((x >> 8) & 0x0000FF00) |
+		((x << 8) & 0x00FF0000) |
+		(x << 24)
+}
+
+func Crc32Bitwise(data []byte, prevCrc32 uint32) uint32 {
+	var crc = int64(prevCrc32) ^ 0xFFFFFFFF
+	for _, c := range data {
+		crc ^= int64(c)
+		for j := 0; j < 8; j++ {
+			crc = (crc >> 1) ^ (-int64(crc&1) & int64(Polynomial))
+		}
+	}
+	return uint32(crc ^ 0xFFFFFFFF)
+}
+
+func Crc32Halfbyte(data []byte, prevCrc32 uint32) uint32 {
+	var crc = int64(prevCrc32) ^ 0xFFFFFFFF
+	for _, c := range data {
+		crc = int64(Crc32Lookup16[(uint32(crc)^uint32(c))&0x0f] ^ (uint32(crc) >> 4))
+		crc = int64(Crc32Lookup16[(uint32(crc)^(uint32(c)>>4))&0x0f] ^ (uint32(crc) >> 4))
+	}
+	return uint32(crc ^ 0xFFFFFFFF)
+}
--- hash_crc_test.go
+++ hash_crc_test.go
@@ -9,6 +9,28 @@ var (
 	crc32In = S2B(`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non lectus dui. Nullam gravida purus libero, sit amet interdum massa pretium viverra. Praesent cursus eu mauris nec rhoncus. Fusce dignissim justo et lorem fermentum eleifend. Sed nisi orci, hendrerit quis mauris vitae, scelerisque blandit risus. Ut imperdiet fermentum diam, vel dapibus velit ornare a. Vivamus non mattis ante. Morbi semper, tortor a convallis rutrum, augue diam ullamcorper ligula, ut luctus nisi orci vitae nunc. Suspendisse dictum porttitor est, id lacinia neque bibendum vel. Suspendisse tristique scelerisque nisi quis consequat. Sed sit amet pulvinar nulla, nec lacinia elit.`)
 )
 
+func BenchmarkCrc32Bitwise(b *testing.B) {
+	h := uint32(0x607650b0)
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		r := Crc32Bitwise(crc32In, 0)
+		if h != r {
+			b.Error(h, "not equal to", r)
+		}
+	}
+}
+
+func BenchmarkCrc32Halfbyte(b *testing.B) {
+	h := uint32(0x607650b0)
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		r := Crc32Halfbyte(crc32In, 0)
+		if h != r {
+			b.Error(h, "not equal to", r)
+		}
+	}
+}
+
 func BenchmarkCrc32NativeSimple(b *testing.B) {
 	h := uint32(0xffffffff)
 	b.ReportAllocs()
