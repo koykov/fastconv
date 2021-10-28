f9e00011ea2f1ce1a30cce305c901ebbe29ae031
Konstantin Voykov <koykov0@gmail.com>
2021-10-28 22:40:52 +0300

    Code fmt issues.

--- bytestr.go
+++ bytestr.go
@@ -7,27 +7,27 @@ import (
 	"unsafe"
 )
 
-// Fast conversion of byte sequence to string.
+// BytesToString makes fast conversion of byte array to string.
 func BytesToString(b []byte) string {
 	return *(*string)(unsafe.Pointer(&b))
 }
 
-// Shorthand alias of BytesToString().
+// B2S is a shorthand alias of BytesToString().
 func B2S(b []byte) string {
 	return BytesToString(b)
 }
 
-// Fast conversion of string to byte sequence.
+// StringToBytes makes fast conversion of string to byte sequence.
 func StringToBytes(s string) []byte {
-	strh := (*reflect.StringHeader)(unsafe.Pointer(&s))
-	var sh reflect.SliceHeader
-	sh.Data = strh.Data
-	sh.Len = strh.Len
-	sh.Cap = strh.Len
-	return *(*[]byte)(unsafe.Pointer(&sh))
+	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	var h reflect.SliceHeader
+	h.Data = sh.Data
+	h.Len = sh.Len
+	h.Cap = sh.Len
+	return *(*[]byte)(unsafe.Pointer(&h))
 }
 
-// Shorthand alias of StringToBytes().
+// S2B is a shorthand alias of StringToBytes().
 func S2B(s string) []byte {
 	return StringToBytes(s)
 }
