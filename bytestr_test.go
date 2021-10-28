f6897ace3bc275efc0d6640a5eef15465046c84d
Konstantin Voykov <koykov0@gmail.com>
2021-10-28 23:00:57 +0300

    Testing issues: use subtests.

--- bytestr_test.go
+++ bytestr_test.go
@@ -6,50 +6,49 @@ import (
 )
 
 func BenchmarkBytesToString(b *testing.B) {
-	p := []byte("foobar")
-	s := "foobar"
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		c := BytesToString(p)
-		if s != c {
-			b.Error(s, "not equal to", s)
+	b.Run("fastconv", func(b *testing.B) {
+		p := []byte("foobar")
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			c := BytesToString(p)
+			if c != "foobar" {
+				b.Errorf("BytesToString mismatch, need %s, got %s", "foobar", c)
+			}
 		}
-	}
-}
-
-func BenchmarkBytesToStringNative(b *testing.B) {
-	b.ReportAllocs()
-	p := []byte("foobar")
-	s := "foobar"
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		c := string(p)
-		if s != c {
-			b.Error(s, "not equal to", s)
+	})
+	b.Run("native", func(b *testing.B) {
+		p := []byte("foobar")
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			c := string(p)
+			if c != "foobar" {
+				b.Errorf("string(bytes) mismatch, need %s, got %s", "foobar", c)
+			}
 		}
-	}
+	})
 }
 
 func BenchmarkStringToBytes(b *testing.B) {
-	p := []byte("foobar")
-	s := "foobar"
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		c := StringToBytes(s)
-		if !bytes.Equal(c, p) {
-			b.Error(c, "not equal to", p)
+	b.Run("fastconv", func(b *testing.B) {
+		p := []byte("foobar")
+		s := "foobar"
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			c := StringToBytes(s)
+			if !bytes.Equal(c, p) {
+				b.Errorf("StringToBytes mismatch, need %s, got %s", "foobar", c)
+			}
 		}
-	}
-}
-
-func BenchmarkStringToBytesNative(b *testing.B) {
-	p := []byte("foobar")
-	s := "foobar"
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		c := []byte(s)
-		if !bytes.Equal(c, p) {
-			b.Error(c, "not equal to", p)
+	})
+	b.Run("native", func(b *testing.B) {
+		p := []byte("foobar")
+		s := "foobar"
+		b.ReportAllocs()
+		for i := 0; i < b.N; i++ {
+			c := []byte(s)
+			if !bytes.Equal(c, p) {
+				b.Errorf("StringToBytes mismatch, need %s, got %s", "foobar", c)
+			}
 		}
-	}
+	})
 }
